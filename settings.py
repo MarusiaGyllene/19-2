35ea78513ba41612e87f2581a9b71479744b4afa
MarusiaGyllene <119393945+MarusiaGyllene@users.noreply.github.com>
2023-01-11 20:50:09 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,7 @@
+valid_email = "qylleneqwerty@qwerty.com"
+valid_password = "12345"
+
+invalid_email = "qqylleneqwerty@qwerty.com"
+invalid_password = "112345"
+
+invalid_key = {"key": "1ea738148a1f19838e1c5d1413877f3691a3731380e733e877b0ae729"}
